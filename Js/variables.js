1f5cb6b5db736102f3a903546eaa41efb51ece74
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-07-20 15:38:30 +0530

    First5lec

---
+++ Js/variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553 // For Fix value
+let accountEmail = "preetpadaliya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accontState;
+
+//accountId = 2222;
+accountEmail = "okok@gmail.com";
+accountPassword = "1818";
+accountCity = "Junagadh";
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accontState])
+
+console.log(accountId);
+console.log(` Hello my id is ${accountId} `); // Right way to write //
+
