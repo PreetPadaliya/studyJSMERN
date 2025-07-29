df29d62bd0e881bcd23d6c1a14521c5b81348c05
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-07-29 18:02:33 +0530

    17

---
+++ Js/object2.js
@@ -0,0 +1,37 @@
+// const tinderUser = new Object();        Singleton Object
+
+
+const tinderUser ={}         // NonSingleton Object
+
+tinderUser.id = "123ab";
+tinderUser.name = "Okayy";
+tinderUser.isLoggedin = false;
+ 
+const regularUser = {
+    email : "nothing@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Preet",
+            lastname : "Padaliya"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj3 = {5:"a",6:"b"}
+
+// const obj = Object.assign({},obj1,obj2,obj3);   //Basic trick to assign the objects
+// const obj = {...obj1 , ...obj2 , ...obj3}          //Spread trick to asssign the objects (We use this)
+// console.log(obj);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
