369cf433765d87b6e606e9997e0fe8445c83ace6
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-12-09 00:01:48 +0530

    rev

---
+++ Js/practice.js
@@ -0,0 +1,19 @@
+// 
+
+//P-2
+
+let marks= 80;
+let grade;
+if(marks <= 49){
+   grade = "F";
+} else if (marks <= 59){
+   grade = "D";
+} else if(marks <= 69){
+    grade = "C"
+} else if(marks <= 89){
+    grade = "B"
+} else if(marks <= 100){
+    grade = "A"
+}
+console.log(grade,"is your grade")
+
