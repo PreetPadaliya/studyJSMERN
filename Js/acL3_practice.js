44bdd938f6347bf5724627c3ba5148fffda9f614
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-12-09 21:34:49 +0530

    rev

--- Js/acL3_practice.js
+++ Js/acL3_practice.js
@@ -1,15 +1,20 @@
-//P-1
-// for(let i = 0 ; i<= 100 ; i++ ){
-//     if(i%2==0){
-//         console.log(i);
-//     }
-// }
+// //P-1
+// // for(let i = 0 ; i<= 100 ; i++ ){
+// //     if(i%2==0){
+// //         console.log(i);
+// //     }
+// // }
 
-//P-2
-let gameNum = 18
-let guessNum = prompt("Guess the number : ");
+// //P-2
+// // let gameNum = 18
+// // let guessNum = prompt("Guess the number : ");
 
-    while(gameNum != guessNum){
-        guessNum = prompt("That's not correct , Guess Again :")
-    }
-    console.log("You Guessed it !")
+// //     while(gameNum != guessNum){
+// //         guessNum = prompt("That's not correct , Guess Again :")
+// //     }
+// //     console.log("You Guessed it !")
+
+// //P-3
+// let name = prompt("Enter your username : ");
+// let str = name.length;
+// console.log("@" + name + str);
