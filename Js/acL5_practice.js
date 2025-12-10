b0308dd61ac02d2c8663cd40b929a556eb79291d
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-12-10 23:39:38 +0530

    rev

--- Js/acL5_practice.js
+++ Js/acL5_practice.js
@@ -32,9 +32,41 @@
 //P-3
 //.forEach() is used for (value , index , array) this is used for traversing the array
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-arr.forEach(element => {
-    element = element**2;
-    console.log(element);
+// arr.forEach(element => {
+//     element = element**2;
+//     console.log(element);
+// })
+
+// console.log(arr); //original array will remain same
+
+//P-4
+
+// let marks = [87 , 93 , 64 , 99 , 86 ]
+
+// const result = marks.filter((marks)=>{
+//     return marks>=90;
+// })
+// console.log(result);
+
+//P-5
+
+let n = prompt("Enter a number : ");
+
+let arr = [];
+
+for(let i = 1 ; i<=n ; i++){
+    arr.push(i);
+}
+console.log(arr);
+
+const sum = arr.reduce((prev, curr)=>{
+    return prev + curr;
 })
+console.log(sum);
+
+const mul = arr.reduce((prev, curr)=>{
+    return prev * curr;
+})
+console.log(mul);   
