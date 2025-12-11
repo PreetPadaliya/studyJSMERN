3ec29f461e23f579496e1600e9bd892ff17ed928
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-12-11 22:28:00 +0530

    commit

---
+++ Js/domPR.js
@@ -0,0 +1,10 @@
+// let butt = document.createElement("button");
+// butt.innerText = "Click Me";
+// butt.style.backgroundColor = "red";
+// butt.style.color = "white";
+// document.querySelector("body").prepend(butt);
+
+document.querySelector("p").classList.add("newClass");
+
+
+ 
