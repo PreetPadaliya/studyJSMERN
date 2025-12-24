125a2b79548cc2de005df5d4ba11a322bb3b52cc
PreetPadaliya <preetpadaliya2805@gmail.com>
2025-12-25 00:28:47 +0530

    Expr-1cmplt

---
+++ MERN/Express/PR_1/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
+
+app.use((req,res,next)=>{
+    console.log("First Middleware",req.url , req.method );
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("Second Middleware",req.url , req.method );
+    next();
+})
+// app.use((req,res,next)=>{
+//     console.log("Second Middleware",req.url , req.method );
+//     res.send('Hello from Express App');
+// })
+app.get('/',(req,res,next)=>{
+    console.log("Came in another middlewrare",req.url , req.method );
+    res.send(`<h1>Welcome to Express App</h1>`);
+});
+
+app.get('/contact-us',(req,res,next)=>{
+    console.log("Came in contact us GET middlewrare",req.url , req.method );
+    res.send(`<h1>Contact Us Page</h1>
+        <form action="/contact-us" method="POST">
+        <input type="text" name="username" placeholder="Enter your name" />
+        <input type="text" name="email" placeholder="Enter your email" />
+        <button type="submit">Submit</button>
+        </form>`);
+});
+
+app.post('/contact-us',(req,res,next)=>{
+    console.log("Came in contact us POST middlewrare",req.url , req.method );
+    res.send('<h3>We will contact you soon!</h3>');
+});
--- MERN/Express/app.js
+++ MERN/Express/app.js
@@ -1,5 +1,3 @@
-//Core Module
-const http = require('http');
 
 //External Module
 const express = require('express');
@@ -9,18 +7,34 @@ const requestHandler = require('./ParsingReq');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("First Middleware",req.url , req.method );
+// app.use((req,res,next)=>{
+//     console.log("First Middleware",req.url , req.method );
+//     next();
+// });
+
+app.get('/',(req,res,next)=>{
+    console.log("Came in first middlewrare",req.url , req.method );
     next();
 });
-app.use((req,res,next)=>{
-    console.log("Second Middleware",req.url , req.method );
-    res.send('Hello from Express App');
+
+app.post('/submit-details',(req,res,next)=>{
+    console.log("Came in second middlewrare",req.url , req.method );
+    res.send('Form Submitted Successfully');
 });
 
-const Srever = http.createServer(app); // Using Express app as request handler , (req,res)
+app.get('/',(req,res,next)=>{
+    console.log("Came in another middlewrare",req.url , req.method );
+    res.send('<h1>Welcome to Express App</h1>');
+});
+
+// app.use((req,res,next)=>{
+//     console.log("Second Middleware",req.url , req.method );
+//     res.send('Hello from Express App');
+// });
+
+// const Srever = http.createServer(app); // Using Express app as request handler , (req,res)
 
     const PORT = 3000;
-Srever.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
 })
