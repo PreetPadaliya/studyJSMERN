c38e3ccda2a88507af7c93c37fa6c62d300942f2
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-01-15 22:14:09 +0530

    Login-reg

--- MERN/Todo-MERN/client/src/pages/Auth/Login.js
+++ MERN/Todo-MERN/client/src/pages/Auth/Login.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './AuthStyle.css'
+
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //login handler function
+    const loginHandler = (e) => { 
+        try {
+          e.preventDefault();
+          alert('Login Successful');
+        } catch (error) {
+          console.log(error);
+        }
+    }
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+   <div className='form-container'>
+    <div className='form'>
+      <div className='mb-3'>
+        <h1 className='Heading'>Login</h1>
+        <div className='mb-3'>
+          <input type="email" className='form-control' placeholder='Enter your email' value={email}
+          onChange={(e)=>setEmail(e.target.value)}/>
+        </div>
+        <div className='mb-3'>
+          <input type="password" className='form-control' placeholder='Enter your password' value={password}
+          onChange={(e)=>setPassword(e.target.value)} />
+        </div>
+        <div className='form-bottom'>
+          <p className='text-center'>
+            not a user ? Please <Link to='/register' className='register-link'>Register</Link>
+          </p>
+          <button type='submit' className='login-btn' onClick={loginHandler}>Login</button>
+        </div>
+      </div>
+      </div>
+   </div>
+
   )
 }
 
--- MERN/Todo-MERN/client/src/pages/Auth/Register.js
+++ MERN/Todo-MERN/client/src/pages/Auth/Register.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+        
+    
+        //register handler function
+        const registerHandler = (e) => { 
+            try {
+              e.preventDefault();
+              alert('Registration Successful');
+            } catch (error) {
+              console.log(error);
+            }
+        }
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <div className='form-container'>
+    <div className='form'>
+      <div className='mb-3'>
+        <h1 className='Heading'>Register</h1>
+        <div className='mb-3'>
+          <input type="text" className='form-control' placeholder='Enter your username' value={username}
+          onChange={(e)=>setUsername(e.target.value)}/>
+        </div>
+        <div className='mb-3'>
+          <input type="email" className='form-control' placeholder='Enter your email' value={email}
+          onChange={(e)=>setEmail(e.target.value)}/>
+        </div>
+        <div className='mb-3'>
+          <input type="password" className='form-control' placeholder='Enter your password' value={password}
+          onChange={(e)=>setPassword(e.target.value)} />
+        </div>
+        <div className='form-bottom'>
+          <p className='text-center'>
+            already registered ? Please <Link to='/login' className='register-link'>Login</Link>
+          </p>
+          <button type='submit' className='login-btn' onClick={registerHandler}>Register</button>
+        </div>
+      </div>
+      </div>
+   </div>
   )
 }
 
