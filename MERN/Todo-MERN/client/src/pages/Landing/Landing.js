9ada01810371f7496be5e1c53e469cc719e700c3
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-01-15 12:15:43 +0530

    Landing Page

--- MERN/Todo-MERN/client/src/pages/Landing/Landing.js
+++ MERN/Todo-MERN/client/src/pages/Landing/Landing.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Hero from '../../assets/images/hero.jpg'
+import './Landing.css'
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className='hero'>
+      <div className='intro-text'>
+        <h1>
+          <span className='tagline1'>Organise your work and life, <br/>
+            <span className='tagline2'>Finally</span>
+          </span>
+        </h1>
+        <p>
+          Welcome to Todo-MERN, your ultimate task management solution built with the power of the MERN stack.
+          Whether you're a busy professional, a student juggling multiple assignments, or someone looking to boost productivity, Todo-MERN has got you covered.
+        </p>
+      <Link className="btn red" to="/register">Register Now!</Link>
+      <Link className="btn blue" to="/login">Login Now!</Link>
+      </div>
+      
+      <div className='hero-image'>
+        <img src={Hero} alt="Hero" width={"100%"} height={"515"} />     
+        </div>
     </div>
   )
 }
