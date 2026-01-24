89a8fafe98d08b5f089ab7d368e8042be933447c
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-01-24 21:34:04 +0530

    auth completed

--- MERN/Todo-MERN/routes/todoRoute.js
+++ MERN/Todo-MERN/routes/todoRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { createTodoController } = require('../controllers/todoController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
-router.post('/create', createTodoController);
+router.post('/create', authMiddleware, createTodoController);
 
 module.exports = router;
