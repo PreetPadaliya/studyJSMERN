fb66db940547e82b933d395cdf0dad751894fcfd
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-06-01 11:33:15 +0530

    API

--- Js/apiFetch.js
+++ Js/apiFetch.js
@@ -1,7 +1,12 @@
-const URL = "https://jsonplaceholder.typicode.com";
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const postPara = document.querySelector("#post");
+
 
 const getFacts = async()=>{
     console.log("getting data...");
     let response = await fetch(URL);
     console.log(response);
+    let data = await response.json();
+    console.log(data[0]);
+    postPara.innerText = data[0].text;
 }
