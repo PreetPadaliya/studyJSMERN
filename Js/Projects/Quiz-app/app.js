32fba16df20193563e9098d52860b1ab390c1ea4
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-06-18 11:22:30 +0530

    quiz

---
+++ Js/Projects/Quiz-app/app.js
@@ -0,0 +1,29 @@
+const quetions = [
+    {
+        question:"What is full form of HTML?",
+        answers:[
+            {text:"Hyper Text Markup Language", correct:true},
+            {text:"Hyper Text Makeup Language", correct:false},
+            {text:"Hydro Text Markover Language", correct:false},
+            {text:"Hyper Text Markup Level", correct:false},
+        ]
+    },
+    {
+        question:"What is full form of CSS?",
+        answers:[
+            {text:"Cascading Style Sheets", correct:true},
+            {text:"Computer Style Sheets", correct:false},
+            {text:"Colorful Style Sheets", correct:false},
+            {text:"Complete Style Sheets", correct:false}
+        ]
+    },
+    {
+        question:"What is full form of JS?",
+        answers:[
+            {text:"JavaScript", correct:true},
+            {text:"Java Style", correct:false},
+            {text:"Just Script", correct:false},
+            {text:"Job Script", correct:false}
+        ]
+    }
+]
