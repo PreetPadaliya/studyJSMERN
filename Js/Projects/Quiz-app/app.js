bf1c2b5e1924e2fa46bb8436a51eee37b24ad73e
PreetPadaliya <preetpadaliya2805@gmail.com>
2026-06-18 17:52:54 +0530

    quiz cmplt

--- Js/Projects/Quiz-app/app.js
+++ Js/Projects/Quiz-app/app.js
@@ -26,4 +26,89 @@ const quetions = [
             {text:"Job Script", correct:false}
         ]
     }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = quetions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        button.addEventListener("click", selectAnswer);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+ 
+nextButton.addEventListener("click", () => {
+    if(nextButton.innerHTML === "Play Again"){
+        startQuiz();
+    }
+    else{
+        currentQuestionIndex++;
+        if(currentQuestionIndex < quetions.length){
+            showQuestion();
+        }
+        else{
+            showScore();
+        }
+    }
+});
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${quetions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+
+startQuiz();
